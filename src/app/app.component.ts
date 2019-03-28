2314922b75e65dc608a366e807c8946c6f67c40b
Will <doggy.huang@gmail.com>
2019-03-29 03:50:47 +0800

    套用 SB Admin 2 的 index.html 版面，並修正所有 JS 錯誤

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { chartAreaDemo } from './chartAreaDemo';
+import { chartPieDemo } from './chartPieDemo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo1';
+
+  ngOnInit(): void {
+    chartAreaDemo();
+    chartPieDemo();
+  }
 }
