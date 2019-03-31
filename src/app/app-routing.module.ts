8e466db207bd093db53ce2df524543e2a31e0641
Will <doggy.huang@gmail.com>
2019-03-31 09:50:23 +0800

    Revert "示範基本路由設定方式"
    This reverts commit fcfc4fdd321b8c51a718a4709689befa364419b0.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
 
-const routes: Routes = [
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component }
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    useHash: true
-  })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Page1Component,
-    Page2Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
