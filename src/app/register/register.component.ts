ff818441d5a336ba06e82be09fb555c2952ed311
Will <doggy.huang@gmail.com>
2019-04-14 11:52:51 +0800

    使用更精細的方式驗證表單欄位

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 class RegisterViewModel {
-  firstName = 'Will';
-  lastName = 'Huang';
+  firstName = '';
+  lastName = '';
   email = '';
   password = '';
   password2 = '';
